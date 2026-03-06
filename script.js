7f56b7185cecc20e40a04b992b5b6ebc42b0ea41
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-03-06 20:13:46 +0800

    refactor Book() to be a class, and make Book() set an id instead of addBookToLibrary

--- script.js
+++ script.js
@@ -9,34 +9,29 @@ const closeForm = document.querySelector(".close");
 const container = document.querySelector(".container");
 
 // write a constructor for making "Book" objects
-function Book(title, author, pages, read) {
+class Book {
+  constructor (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID(); // set id to a random unique id
+  }
+
+  info () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+// create a function that can take some arguments, create a book from those arguments, and store the new book object into an array
+function addBookToLibrary(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  };
-};
-
-// create a function that can take some arguments, create a book from those arguments, and store the new book object into an array
-function addBookToLibrary(title, author, pages, read) {
-  Book.call(this, title, author, pages, read);
-
-  const id = crypto.randomUUID(); // create a random unique id
-
-  // create a book object that stores the book's id, title, author, number of pages, and read status
-  const book = {
-    id: id,
-    title: this.title,
-    author: this.author,
-    pages: this.pages,
-    read: this.read
-  };
-
-  // push the book object into myLibrary array
-  myLibrary.push(book);
+  
+  const book = new Book(this.title, this.author, this.pages, this.read); // create a book with the parameters
+  myLibrary.push(book); // then push the book to myLibrary
 };
 
 // Link prototypes and add prototype methods
